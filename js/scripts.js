b9a31ace0df314a8c82abba987b5bd4aded21ffe
mellowmorgan <morgan.waites@gmail.com>
2021-10-29 08:43:06 -0700

    get test 0 to pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 
 
-
 function beepBoop(number){
+  let numberArray = [];
+  for (let i=0;i<number+1;i++){
+    numberArray.push(i);
+  }
+  
+  return numberArray;
 
 }
-  return 
-
 
 $(document).ready(function(){
   $("form#user").submit(function(event){
